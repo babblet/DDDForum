de376f028f8d1d9d64b2c0e632aa5b5bf377fec0
babblet <frk.haha@hotmail.se>
2024-11-11 08:04:27 +0100

    Just make it work

---
+++ backend/src/index.ts
@@ -0,0 +1,244 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+import { database } from "./database";
+import type { User } from "./database";
+
+// Errors that can be returned by the API
+enum Errors {
+  UsernameAlreadyTaken = "UserNameAlreadyTaken",
+  EmailAlreadyInUse = "EmailAlreadyInUse",
+  ValidationError = "ValidationError",
+  ServerError = "ServerError",
+  ClientError = "ClientError",
+  UserNotFound = "UserNotFound",
+}
+
+// Used HTTP Status Codes in this API
+enum HTTPStatusCodes {
+  Ok = 200,
+  Created = 201,
+  BadRequest = 400,
+  NotFound = 404,
+  Conflict = 409,
+  InternalServerError = 500,
+}
+
+// We do not care about the passwords right now, so we can generate a random password
+function generateRandomPassword(length: number): string {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+  const passwordArray = [];
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    passwordArray.push(charset[randomIndex]);
+  }
+
+  return passwordArray.join("");
+}
+
+// We don't want to return the password within the request
+function parseUserForResponse(user: User) {
+  const returnData = JSON.parse(JSON.stringify(user));
+  delete returnData.password;
+  return returnData;
+}
+
+const app = express();
+app.use(express.json());
+
+// Create a new user
+app.post("/users/new", async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+
+    // Check if the user data is valid
+    if (
+      !userData.email ||
+      !userData.username ||
+      !userData.firstName ||
+      !userData.lastName
+    ) {
+      return res.status(HTTPStatusCodes.BadRequest).json({
+        error: Errors.ValidationError,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    // Check if the user exists by email or not
+    const existingUserByEmail = await database.user.findFirst({
+      where: { email: userData.email },
+    });
+    if (existingUserByEmail) {
+      return res.status(HTTPStatusCodes.Conflict).json({
+        error: Errors.EmailAlreadyInUse,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    // Check if the user exists by username or not
+    const existingUserByUsername = await database.user.findFirst({
+      where: { username: userData.username },
+    });
+    if (existingUserByUsername) {
+      return res.status(HTTPStatusCodes.Conflict).json({
+        error: Errors.UsernameAlreadyTaken,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    const user = await database.user.create({
+      data: { ...userData, password: generateRandomPassword(10) },
+    });
+
+    return res.status(HTTPStatusCodes.Ok).json({
+      error: undefined,
+      data: parseUserForResponse(user),
+      success: true,
+    });
+  } catch (error) {
+    // Return a failure error response
+    return res.status(HTTPStatusCodes.InternalServerError).json({
+      error: "ServerError",
+      data: undefined,
+      success: false,
+    });
+  }
+});
+
+// Edit a user
+app.post("/users/edit/:userId", async (req: Request, res: Response) => {
+  try {
+    const userId = Number(req.params.userId);
+
+    if (isNaN(userId)) {
+      return res.status(HTTPStatusCodes.BadRequest).json({
+        error: Errors.ValidationError,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    // Check if user exists
+    const userToEdit = await database.user.findUnique({ where: { id: userId } });
+    if (!userToEdit) {
+      return res
+        .status(HTTPStatusCodes.NotFound)
+        .json({ error: Errors.UserNotFound, data: undefined, success: false });
+    }
+
+    const postedUserData = req.body;
+
+    // Check if the user data is valid
+    if (
+      !postedUserData.email ||
+      !postedUserData.username ||
+      !postedUserData.firstName ||
+      !postedUserData.lastName
+    ) {
+      return res.status(HTTPStatusCodes.BadRequest).json({
+        error: Errors.ValidationError,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    // Check if a user with the new email exists
+    const existingUserByEmail = await database.user.findFirst({
+      where: {
+        email: postedUserData.email,
+        id: {
+          not: userId, // Exclude the current user
+        },      
+      },
+    });
+    if (existingUserByEmail) {
+      return res.status(HTTPStatusCodes.Conflict).json({
+        error: Errors.EmailAlreadyInUse,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    // Check if a user with the new username exists
+    const existingUserByUsername = await database.user.findFirst({
+      where: {
+        username: postedUserData.username,
+        id: {
+          not: userId // Exclude the current user
+        }
+      },
+    });
+    if (existingUserByUsername) {
+      return res.status(HTTPStatusCodes.Conflict).json({
+        error: Errors.UsernameAlreadyTaken,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    const updatedUser: User = await database.user.update({
+      where: { id: userToEdit.id },
+      data: postedUserData,
+    });
+
+    return res.status(HTTPStatusCodes.Ok).json({
+      error: undefined,
+      data: parseUserForResponse(updatedUser),
+      success: true,
+    });
+  } catch (error) {
+    // Return a failure error response
+    return res.status(HTTPStatusCodes.InternalServerError).json({
+      error: "ServerError",
+      data: undefined,
+      success: false,
+    });
+  }
+});
+
+// Get a user by email
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const userEmailFromParams = req.query.email as string;
+
+    // Check that the email is not empty
+    if (!userEmailFromParams) {
+      return res.status(HTTPStatusCodes.BadRequest).json({
+        error: Errors.ValidationError,
+        data: undefined,
+        success: false,
+      });
+    }
+
+    // Check if user exists
+    const userFoundByEmail = await database.user.findFirst({ where: { email: userEmailFromParams } });
+    if (!userFoundByEmail) {
+      return res
+        .status(HTTPStatusCodes.NotFound)
+        .json({ error: Errors.UserNotFound, data: undefined, success: false });
+    }
+
+    return res.status(HTTPStatusCodes.Ok).json({
+      error: undefined,
+      data: parseUserForResponse(userFoundByEmail),
+      success: true,
+    });
+  } catch (error) {
+    // Return a failure error response
+    return res.status(HTTPStatusCodes.InternalServerError).json({
+      error: "ServerError",
+      data: undefined,
+      success: false,
+    });
+  }
+});
+
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
